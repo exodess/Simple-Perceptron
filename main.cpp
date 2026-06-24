ed2165ae8e1d44ee73049fefea17ef5a3a70dc2d
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-06-24 15:51:52 +0400

    Added compilation of tools

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+#include "tools/tools.h"
+
 int main() {
+    ImageReader bmp_reader;
     std::cout << "Hello, World!" << std::endl;
 
+    bmp_reader.Read("../materials/sample-bmp-files-sample_640x426.bmp");
     return 0;
 }
