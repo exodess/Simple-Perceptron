2d089a93c2db445151825fa08f64416be1ffe1da
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-07-02 17:58:03 +0300

    Changed the structure of the matrix perceptron class

--- include/perceptron/matrix_perceptron.h
+++ include/perceptron/matrix_perceptron.h
@@ -7,41 +7,53 @@
  * @author Tarasova Alina
  */
 
+#include <data/data.h>
 #include <perceptron/perceptron.h>
 
 namespace perc {
 
     /**
-     * @class Matrix_perceptron
+     * @class MatrixPerceptron
      * @brief Класс, описывающий основные методы
      * обучения и работы перцептрона в матричной реализации
      */
-    class Matrix_perceptron : public Perceptron {
+    class MatrixPerceptron final : public Perceptron {
     public:
-        explicit Matrix_perceptron(int number, int hidden_layer_sizes) noexcept;
-
-        explicit Matrix_perceptron(int number, int hidden_layer_sizes, vector<float> weights) noexcept;
-
-        ~Matrix_perceptron() = default;
+        explicit MatrixPerceptron(int number, int hidden_layer_sizes) noexcept;
+        ~MatrixPerceptron() = default;
 
         /**
-         * @brief Метод, который устанавливает количество скрытых слоев
-         * @param value количество скрытых эпох
+         * @brief Приводит перцептрон к начальному состонию
          */
-        void setHiddenLayer(int value) noexcept override;
+        void Reset() noexcept override;
 
         /**
-         * @brief Метод, который устанавливает новые данные для перцептрона.
-         * @param vector<float> normalize_input вектор входных значений
+         * @brief Метод, который делает предсказание по переданному изображению (данными)
+         * @param image вектор входных значений
          */
-        void setDataInput(const vector<float>& normalize_input) noexcept;
+        int Verify(const vector<float>& image) noexcept override;
 
+        /**
+         * @brief Метод, который обучает перцептрон на выборке
+         * @param dataset вектор объектов EmnistData,
+         * которые содержат вектор входных значений(изображения) и индекс буквы
+         */
+        float Train(const std::vector<EmnistData>& dataset) noexcept override;
+
+        /**
+         * @brief Осуществляет доступ к весам перцептрона для того,
+         * чтобы загрузить их в файл с помощью DataReader или сохранить в перцептроне
+         * @return Массив всех весов
+         */
+        vector<float>& Weights() noexcept override;
+
+    private:
         /**
          * @brief Метод, который делает полный проход по слоям перцептрона,
          * суммирует выходы нейронов предыдущего слоя, умноженные на текущие веса.
          * На последнем R слое получаются 26 нейронов с выходным значением для каждого (outputs_).
          */
-        void sumFunc() noexcept;
+        void _sumFunc() noexcept;
 
         /**
          * @brief Метод, который делает полный проход по слоям в обратном направлении
@@ -51,60 +63,23 @@ namespace perc {
          * для текущего нейрона выход.
          * Используется только для обучения
          */
-        float backPropagation(float expected_[OUTPUT_SIZE]) noexcept;
+        float _backPropagation(const vector<float>& expected) noexcept;
 
         /**
          * @brief Метод, который задает рандомные значения весам
          */
-        void updateWeights() noexcept;
+        void _updateWeights() noexcept;
 
-        /**
-         * @brief Метод, который делает предсказание по переданному изображению (данными)
-         * @param const vector<float>& image вектор входных значений
-         */
-        int verify(const vector<float>& image) noexcept override;
-
-        /**
-         * @brief Метод, который обучает перцептрон на выборке
-         * @param epoch количество эпох
-         * @param std::vector<EmnistData>& EmnistData_ вектор объектов EmnistData,
-         * которые содержат вектор входных значений(изображения) и индекс буквы
-         */
-        vector<float> training(std::vector<EmnistData> EmnistData_, int epoch) noexcept override;
-
-        /**
-        * @brief Метод, который тестирует перцептрон на тестовой выборке
-        * @param percentage соотношение тестируемой выборки, задается дробным числом
-        */
-        void experiment(float percentage) noexcept override;
-
-        void loadWeights(const vector<float>& data) noexcept override;
-
-        vector<float> saveWeights() noexcept override;
-
-        PerceptronType& type() noexcept override;
-
-        int number() noexcept override;
-
-        int hidden_layers() noexcept override;
-
-    private:
         float y_[OUTPUT_SIZE]{}; ///< вектор ожидаемых значений (все значения равны нулю кроме y_[i], где i - индекс буквы)
         int number_; ///< Индекс буквы
 
-        int layers_count; ///< Количество всех слоев перцептрона
         int hidden_layers_count_; ///< Количество скрытых слоев
-        int epochs_; ///< Количество эпох обучения
 
         vector<Layer> layers_; ///< вектор всех слоев нейрона, где layers_[0] - входной слой, layers_[layers_count - 1] - выходной
         vector<float> normalize_input_; ///< вектор входных значений для одного изображения
 
         vector<EmnistData> test_set; ///< тестовая выборка для обучения и эксперимента, формурется в training
         vector<EmnistData> training_set; ///< тренировочная выборка
-
-        float learning_rate_{0.1}; ///< Шаг обучения
-
-        PerceptronType Perceptron_type_{MATRIX_VIEW}; ///< Тип реализации
     };
 }
 
