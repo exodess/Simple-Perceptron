334524fff5ebca5a9a23d81f8658f900f359dd33
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-07-02 19:16:53 +0300

    Wrapped Neuron class in the perc namespace

--- perceptron/matrix/matrix_neuron.cpp
+++ perceptron/matrix/matrix_neuron.cpp
@@ -1,7 +1,10 @@
-#include "../../include/perceptron/perceptron.h"
+#include "perceptron/perceptron.h"
+#include <cmath>
 
-Neuron::Neuron() noexcept: deltas_{}, outputs_{} {}
+namespace perc {
+    Neuron::Neuron() noexcept: deltas_{}, outputs_{}, bias_{} {}
 
-float Neuron::sigmoidalFunc(float x) noexcept {
-    return  1. / (1. + exp(-x));
+    float Neuron::sigmoidalFunc(float x) noexcept {
+        return 1. / (1. + exp(-x));
+    }
 }
