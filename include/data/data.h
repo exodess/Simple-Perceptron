7dc709b65f58b30e4b65cf7c4f062997d6501d08
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-07-03 19:54:46 +0300

    Added constants for the number of groups (k) and hidden layers

--- include/data/data.h
+++ include/data/data.h
@@ -17,6 +17,12 @@
 // Настройки перцептрона
 #define INPUT_SIZE DEFAULT_WIDTH_RES * DEFAULT_HEIGHT_RES
 #define OUTPUT_SIZE COUNT_LETTERS
+#define MIN_K 1
+#define DEFAULT_K 5 ///< Значение k для кросс-валидации
+#define MAX_K 20
+#define MIN_COUNT_EPOCHS 10
+#define DEFAULT_COUNT_EPOCHS 30
+#define MAX_COUNT_EPOCHS 120
 #define MIN_HIDDEN 2 ///< Минимальное количество скрытых слоев перцептрона
 #define MAX_HIDDEN 5 ///< Максимальное количество скрытых слоев перцептрона
 #define MAX_NEURONS 64
