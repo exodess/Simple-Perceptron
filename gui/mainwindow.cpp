eef15680640c4199d46fd50edf68ebb6fc8ee0cc
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-07-03 19:57:56 +0300

    Added signal coupling and processing in separate functions

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -1,5 +1,5 @@
 #include "gui/mainwindow.h"
-#include <data/data.h>
+#include "data/data.h"
 #include <QFileDialog>
 #include <QMainWindow>
 
@@ -9,6 +9,8 @@ namespace gui {
     , ui_{std::make_unique<GUI_MainWindow>()}
     , controller_(std::make_unique<perc::Controller>())
     , part_sample_(1.0f)
+    , k_(DEFAULT_K)
+    , count_epochs_(DEFAULT_COUNT_EPOCHS)
     {
         setWindowTitle("Перцептрон");
         resize(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
@@ -21,22 +23,32 @@ namespace gui {
         qDebug() << "[MainWindow] Инициализация завершена";
     }
 
-    void MainWindow::connectSignals() noexcept {
+    void MainWindow::connectSignals() const noexcept {
+        // Соединяем все сигналы
+
         connect(ui_->group_NavigationButtons, &QButtonGroup::idClicked, this, &MainWindow::on_btn_NavigationButton_clicked);
         connect(ui_->btn_LoadSample, &QPushButton::clicked, this, &MainWindow::on_btn_LoadSample_clicked);
-        connect(ui_->btn_StartSample, &QPushButton::clicked, this, &MainWindow::on_btn_StartSample_clicked);
 
-        // Ползунок
         connect(ui_->slider_Sample, &QSlider::valueChanged, this, &MainWindow::on_slider_Sample_valueChanged);
+        connect(ui_->btn_StartSample, &QPushButton::clicked, this, &MainWindow::on_btn_StartTesting_clicked);
+
+        connect(ui_->group_TrainNav, &QButtonGroup::idClicked, this, &MainWindow::on_btn_SubNavigationButton_clicked);
+        connect(ui_->spin_EpochsGroups, &QSpinBox::valueChanged, this, &MainWindow::on_spin_CountEpochs_valueChanged);
+        connect(ui_->btn_StartNormalTrain, &QPushButton::clicked, this, &MainWindow::on_btn_StartNormalTraining_clicked);
+        connect(ui_->spin_KGroups, &QSpinBox::valueChanged, this, &MainWindow::on_spin_CrossValidationK_valueChanged);
+        connect(ui_->btn_StartCrossValTrain, &QPushButton::clicked, this, &MainWindow::on_btn_StartCrossValidationTraining_clicked);
+
+        connect(ui_->combo_ImplMenu, &QComboBox::currentIndexChanged, this, &MainWindow::on_combo_PerceptronType_indexChanged);
+        connect(ui_->spin_HiddenLayers, &QSpinBox::valueChanged, this, &MainWindow::on_spin_CountHiddenLayers_valueChanged);
+        connect(ui_->btn_SaveWeights, &QPushButton::clicked, this, &MainWindow::on_btn_SaveWeights_clicked);
+        connect(ui_->btn_LoadWeights, &QPushButton::clicked, this, &MainWindow::on_btn_LoadWeights_clicked);
     }
 
-    void MainWindow::on_slider_Sample_valueChanged() {
-        part_sample_ = static_cast<float>(ui_->slider_Sample->value()) / 100.0;
-
-        qDebug() << "Установлено значение на слайдере: " << part_sample_;
+    void MainWindow::on_btn_NavigationButton_clicked(int index) noexcept {
+        ui_->widget_ContentStack->setCurrentIndex(index);
     }
 
-    void MainWindow::on_btn_LoadSample_clicked() {
+    void MainWindow::on_btn_LoadSample_clicked() noexcept {
         QString fileName = QFileDialog::getOpenFileName(
             this, "Открыть тестовую выборку", QString(),
             "CVS Files (*.cvs);;All Files (*)");
@@ -51,7 +63,11 @@ namespace gui {
         }
     }
 
-    void MainWindow::on_btn_StartSample_clicked() {
+    void MainWindow::on_slider_Sample_valueChanged() noexcept {
+        part_sample_ = static_cast<float>(ui_->slider_Sample->value()) / 100.0;
+    }
+
+    void MainWindow::on_btn_StartTesting_clicked() noexcept {
         if (!current_test_sample_.isEmpty()) {
             qDebug() << "Начало прогона тестовой выборки\n";
 
@@ -61,8 +77,79 @@ namespace gui {
         }
     }
 
-    void MainWindow::on_btn_NavigationButton_clicked(int index){
-        ui_->widget_ContentStack->setCurrentIndex(index);
+    void MainWindow::on_btn_SubNavigationButton_clicked(int index) noexcept {
+        ui_->widget_TrainStack->setCurrentIndex(index);
     }
+
+    void MainWindow::on_spin_CountEpochs_valueChanged() noexcept {
+        count_epochs_ = ui_->spin_EpochsGroups->value();
+    }
+
+    void MainWindow::on_btn_StartNormalTraining_clicked() noexcept {
+        auto graphic_data = controller_->training(count_epochs_);
+
+        // Построение графика
+    }
+
+    void MainWindow::on_spin_CrossValidationK_valueChanged() noexcept {
+        k_ = ui_->spin_KGroups->value();
+    }
+
+    void MainWindow::on_btn_StartCrossValidationTraining_clicked() noexcept {
+        auto res = controller_->crossValidation(k_);
+
+        // Вывод метрик из res на экран
+    }
+
+    void MainWindow::on_combo_PerceptronType_indexChanged(int index) noexcept {
+        perc::PerceptronType type;
+
+        if (index == 0) {
+            type = perc::MATRIX_VIEW;
+
+            qDebug() << "Выбрана матричная реализация перцептрона";
+        }
+        else {
+            type = perc::GRAPH_VIEW;
+
+            qDebug() << "Выбрана графовая реализация перцептрона";
+        }
+
+        controller_->switchImplementation(type);
+    }
+
+    void MainWindow::on_spin_CountHiddenLayers_valueChanged() noexcept {
+        int count = ui_->spin_HiddenLayers->value();
+
+        controller_->switchHiddenLayers(count);
+    }
+
+    void MainWindow::on_btn_SaveWeights_clicked() noexcept {
+        QString fileName = QFileDialog::getOpenFileName(
+            this, "Выберите файл для сохранения весов", QString(),
+            "Weights File (*.weights);;All Files (*)");
+
+        if (!fileName.isEmpty()) {
+            qDebug() << "Веса сохранены в файле " << fileName;
+
+            current_test_sample_ = fileName;
+            controller_->saveWeights();
+        }
+    }
+
+    void MainWindow::on_btn_LoadWeights_clicked() noexcept {
+        QString fileName = QFileDialog::getOpenFileName(
+            this, "Открыть файл с весами", QString(),
+            "Weights File (*.weights);;All Files (*)");
+
+        if (!fileName.isEmpty()) {
+            qDebug() << "Загружен файл " << fileName;
+
+            current_test_sample_ = fileName;
+            controller_->open(fileName.toStdString());
+        }
+    }
+
+
 
 }
