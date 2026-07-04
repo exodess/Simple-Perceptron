5dafa8b7119f8a7330ab99ffbfb8e52c4a8c3bf3
pugovka <destinyshesaid@yandex.ru>
2026-07-04 16:32:42 +0300

    Fix vector operations for Matrix and Graph classes

--- perceptron/graph/graph_perceptron.cpp
+++ perceptron/graph/graph_perceptron.cpp
@@ -174,7 +174,7 @@ float Graph_perceptron::Train(const std::vector<EmnistData>& data) noexcept {
         
         float y_training[OUTPUT_SIZE]{};
 
-        y_training[input.index() - 1] = 1;
+        y_training[input.index()] = 1;
 
         sumFunc(input.data());
         epoch_loss += backPropagation(y_training);
