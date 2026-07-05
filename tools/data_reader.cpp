34db02913b2d2b53b349b6c190d30a8e69fb5579
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-07-05 13:16:49 +0300

    Changed the loading and reading of weights via a binary file

--- tools/data_reader.cpp
+++ tools/data_reader.cpp
@@ -7,7 +7,7 @@ namespace perc {
         std::vector<float> result_data;
 
         std::ifstream file;
-        file.open(path_to_file);
+        file.open(path_to_file, std::ios::binary);
 
         if (!file.is_open()) {
 #ifdef DEBUG
@@ -16,11 +16,24 @@ namespace perc {
             throw std::runtime_error("The file does not exist");
         }
 
-        // Пока что примерный код, отвечающий за считывание данных
-        while (!file.eof()) {
-            int value;
-            file >> value;
-            result_data.push_back(value);
+        // Расчет количества элементов в файле
+        file.seekg(0, std::ios::end);
+        long count_element = file.tellg() / sizeof(float);
+        result_data.reserve(count_element);
+
+        const long step = 256; // Количество ячеек, считываемых за один раз
+        char buffer[sizeof(float) * step];
+
+        // Читаем по count значений float с начала файла
+        file.seekg(std::ios::beg);
+        while (count_element != 0) {
+            // Количество ячеек float, которые считаются на этом шагу
+            std::size_t count = std::min(step, count_element);
+            file.read(buffer, count * sizeof(float));
+
+            result_data.insert(result_data.end(), buffer, buffer + count * sizeof(float));
+
+            count_element -= count;
         }
 
         file.close();
@@ -30,7 +43,7 @@ namespace perc {
 
     void DataReader::saveData(const std::string &path, const std::vector<float> &data) noexcept {
         std::ofstream file;
-        file.open(path);
+        file.open(path, std::ios::binary);
 
         if (!file.is_open()) {
 #ifdef DEBUG
@@ -39,9 +52,20 @@ namespace perc {
             throw std::runtime_error("Couldn't open the file");
         }
 
-        // Тоже примерный код для сохранения массива данных
-        for (auto v : data) {
-            file << v;
+        const long step = 256; // Количество ячеек, считываемых за один раз
+        long size_data = data.size();
+        char buffer[sizeof(float) * step];
+        auto it = data.begin();
+
+        // Считываем по count значений float из вектора data
+        while (size_data != 0) {
+            std::size_t count = std::min(size_data, step);
+
+            std::copy(it, it + count, buffer);
+            file.write(buffer, count * sizeof(float));
+
+            size_data -= count;
+            it += count;
         }
 
         file.close();
