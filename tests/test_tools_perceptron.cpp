7a7470eb31f276c12cab1f7df744703afa90f509
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-07-08 19:15:15 +0300

    Added a test for the DataReader class

---
+++ tests/test_tools_perceptron.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include "perceptron/graph_perceptron.h"
+#include "tools/tools.h"
+
+TEST(DataReaderTest, test_1) {
+    perc::Graph_perceptron perceptron(2);
+    perc::DataReader reader;
+
+    auto init_weigths = perceptron.GetWeights();
+
+    reader.saveData(".temp", init_weigths);
+
+    reader.Read(".temp");
+    auto save_data = reader.data();
+
+    EXPECT_EQ(init_weigths.size(), save_data.size());
+    EXPECT_EQ(init_weigths == save_data, true);
+
+}
