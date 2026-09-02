763f0f54687a74d14db72d2ffef21e436dcbd1eb
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-09-02 15:46:42 +0300

    Added testing of the DataReader class with Matrix perceptron

--- tests/test_tools_perceptron.cpp
+++ tests/test_tools_perceptron.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "perceptron/graph_perceptron.h"
+#include "perceptron/matrix_perceptron.h"
 #include "tools/tools.h"
 
 TEST(DataReaderTest, test_1) {
@@ -15,5 +16,19 @@ TEST(DataReaderTest, test_1) {
 
     EXPECT_EQ(init_weigths.size(), save_data.size());
     EXPECT_EQ(init_weigths == save_data, true);
+}
 
+TEST(DataReaderTest, test_2) {
+    perc::Matrix_perceptron perceptron(2);
+    perc::DataReader reader;
+
+    auto init_weigths = perceptron.GetWeights();
+
+    reader.saveData(".temp", init_weigths);
+
+    reader.Read(".temp");
+    auto save_data = reader.data();
+
+    EXPECT_EQ(init_weigths.size(), save_data.size());
+    EXPECT_EQ(init_weigths == save_data, true);
 }
